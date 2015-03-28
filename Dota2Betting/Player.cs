f0c33e05371a13ef87b0ae8f1d09e9f31cee6283
Matthew R <matthewrz52@gmail.com>
2015-03-29 00:15:32 +1100

    First commit.

---
+++ Dota2Betting/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dota2Betting
+{
+    public enum Teams
+    {
+
+    }
+
+    public enum Heroes
+    {
+
+    }
+
+    public class Player
+    {
+        public string Name;
+        public Teams Team;
+    }
+
+    public class MatchPlayer
+    {
+        public Player Player;
+        public Heroes Hero;
+        public int GPM;
+        public int XPM;
+
+    }
+}
